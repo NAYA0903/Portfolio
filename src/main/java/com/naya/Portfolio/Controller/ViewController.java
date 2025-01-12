4f8cc4f6f93dd3bc37a277827942cf0023f34bcf
NAYA <pianoman0903@naver.com>
2025-01-12 23:45:32 +0900

    123

--- src/main/java/com/naya/Portfolio/Controller/ViewController.java
+++ src/main/java/com/naya/Portfolio/Controller/ViewController.java
@@ -1,9 +1,9 @@
 package com.naya.Portfolio.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ViewController {
 	
 	@GetMapping("/naya0903")
